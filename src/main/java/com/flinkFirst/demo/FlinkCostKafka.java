adc7ade198cb0780d41c84d9836be6a10d9bd767
胡思彬 <1144075799@qq.com>
2019-05-14 18:27:33 +0800

    Kafka链接Flink

---
+++ src/main/java/com/flinkFirst/demo/FlinkCostKafka.java
@@ -0,0 +1,55 @@
+package com.flinkFirst.demo;
+
+import java.util.Properties;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.ProcessingTimeSessionWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer08;
+import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.util.Collector;
+
+public class FlinkCostKafka {
+
+
+    public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.enableCheckpointing(1000);
+
+
+        DataStream<Tuple2<String, Integer>> counts = null;
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("zookeeper.connect", "127.0.0.1:2181");
+        properties.setProperty("group.id", "flink-windows");
+
+        FlinkKafkaConsumer08<String> myConsumer = new FlinkKafkaConsumer08<String>("flink-windows", new SimpleStringSchema(),
+                properties);
+
+        DataStream<String> stream = env.addSource(myConsumer);
+
+        counts= stream.flatMap(new LineSplitter()).keyBy(0).window(ProcessingTimeSessionWindows.withGap(Time.seconds(2))).sum(1);
+
+        counts.print().setParallelism(1);
+
+        env.execute("FlinkCostKafka");
+    }
+
+    public static final class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        private static final long serialVersionUID = 1L;
+
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            String[] tokens = value.toLowerCase().split("\\W+");
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<String, Integer>(token, 1));
+                }
+            }
+        }
+    }
+
+}
